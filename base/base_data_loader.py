49d31dc23c45d6c6e38fce2a74fb3d459a61321d
zhuBoDian <379837872@qq.com>
2022-05-09 23:05:09 +0800

    添加指定验证集的功能（可以用来实现train与val不同的transform，详细例子看AlexNet）

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -4,11 +4,14 @@ from torch.utils.data.dataloader import default_collate
 from torch.utils.data.sampler import SubsetRandomSampler
 from sklearn.model_selection import StratifiedShuffleSplit
 
+
 class BaseDataLoader(DataLoader):
     """
     Base class for all data loaders
     """
-    def __init__(self, dataset, batch_size, shuffle, validation_split, num_workers, collate_fn=default_collate):
+
+    def __init__(self, dataset, batch_size, shuffle, validation_split, num_workers, collate_fn=default_collate,
+                 assigned_val=False, samplers=None):
         """
         @param dataset: torchvision.datasets的实例化类
         @param batch_size:
@@ -19,11 +22,18 @@ class BaseDataLoader(DataLoader):
         """
         self.validation_split = validation_split
         self.shuffle = shuffle
+        self.assigned_val = assigned_val
 
         self.batch_idx = 0
         self.n_samples = len(dataset)
 
-        self.sampler, self.valid_sampler = self._split_sampler(self.validation_split)
+        assert (not assigned_val) or (assigned_val and samplers is not None), print('令assigned_val=False，或给定训练集、验证集samples')
+        # 如果事先指定好验证集的索引，就不再随机指定了
+        if not self.assigned_val:
+            self.sampler, self.valid_sampler = self._split_sampler(self.validation_split, self.assigned_val)
+        else:
+            self.sampler, self.valid_sampler = samplers[0], samplers[1]
+            self._split_sampler(self.validation_split, self.assigned_val)
 
         self.init_kwargs = {
             'dataset': dataset,
@@ -34,12 +44,12 @@ class BaseDataLoader(DataLoader):
         }
         super().__init__(sampler=self.sampler, **self.init_kwargs)
 
-    def _split_sampler(self, split):
+    def _split_sampler(self, split, assigned_val):
         """这个就是随机抽样，mnist各类别均匀，随机抽样倒是也行，但是对于不均匀样本还是分层抽样"""
         if split == 0.0:
             return None, None
 
-        """原代码是随机抽样，自己改为分层抽样
+        """# 原代码是随机抽样，自己改为分层抽样
         idx_full = np.arange(self.n_samples)
 
         np.random.seed(0)
@@ -63,14 +73,17 @@ class BaseDataLoader(DataLoader):
         self.n_samples = len(train_idx)
         """
 
-        split = StratifiedShuffleSplit(n_splits=1, test_size=split, random_state=42)
-        for train_index, valid_index in split.split(np.array(self.dataset.data), np.array(self.dataset.targets)):
-            train_sampler, valid_sampler = SubsetRandomSampler(train_index), SubsetRandomSampler(valid_index)
+        if not assigned_val:
+            split = StratifiedShuffleSplit(n_splits=1, test_size=split, random_state=42)
+            for train_index, valid_index in split.split(np.array(self.dataset.data), np.array(self.dataset.targets)):
+                train_sampler, valid_sampler = SubsetRandomSampler(train_index), SubsetRandomSampler(valid_index)
+                self.shuffle = False
+                self.n_samples = len(train_index)
+                return train_sampler, valid_sampler
+        else:
+            self.shuffle = False
+            self.n_samples = len(self.dataset.samples[0])
 
-        self.shuffle = False
-        self.n_samples = len(train_index)
-
-        return train_sampler, valid_sampler
 
     def split_validation(self):
         if self.valid_sampler is None:
