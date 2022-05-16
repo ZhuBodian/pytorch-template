8deeeafbc4adec516a48ffe2b9ddecd4b610dd3a
zhuBoDian <379837872@qq.com>
2022-05-16 23:03:31 +0800

    优化

--- utils/util.py
+++ utils/util.py
@@ -6,26 +6,32 @@ from itertools import repeat
 from collections import OrderedDict
 from utils import global_var
 import time
+import pickle
+
 
 def ensure_dir(dirname):
     dirname = Path(dirname)
     if not dirname.is_dir():
         dirname.mkdir(parents=True, exist_ok=False)
 
+
 def read_json(fname):
     fname = Path(fname)
     with fname.open('rt') as handle:
         return json.load(handle, object_hook=OrderedDict)
+
 
 def write_json(content, fname):
     fname = Path(fname)
     with fname.open('wt') as handle:
         json.dump(content, handle, indent=4, sort_keys=False)
 
+
 def inf_loop(data_loader):
     ''' wrapper function for endless data loader. '''
     for loader in repeat(data_loader):
         yield from loader
+
 
 def prepare_device(n_gpu_use):
     """
@@ -35,13 +41,15 @@ def prepare_device(n_gpu_use):
     if n_gpu_use > 0 and n_gpu == 0:
         print("Warning: There\'s no GPU available on this machine,"
               "training will be performed on CPU.")
-        global_var.get_value('email_log').add_log("Warning: There\'s no GPU available on this machine,training will be performed on CPU.")
+        global_var.get_value('email_log').add_log(
+            "Warning: There\'s no GPU available on this machine,training will be performed on CPU.")
 
         n_gpu_use = 0
     if n_gpu_use > n_gpu:
         print(f"Warning: The number of GPU\'s configured to use is {n_gpu_use}, but only {n_gpu} are "
               "available on this machine.")
-        global_var.get_value('email_log').add_log(f"Warning: The number of GPU\'s configured to use is {n_gpu_use}, but only {n_gpu} are available on this machine.")
+        global_var.get_value('email_log').add_log(
+            f"Warning: The number of GPU\'s configured to use is {n_gpu_use}, but only {n_gpu} are available on this machine.")
 
         n_gpu_use = n_gpu
     device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
@@ -49,8 +57,21 @@ def prepare_device(n_gpu_use):
     return device, list_ids
 
 
+def save_as_pickle(name, data):
+    with open(name, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_from_pickle(name):
+    assert os.path.exists(name), print(f'文件{name}不存在')
+    with open(name, "rb") as fh:
+        data = pickle.load(fh)
+    return data
+
+
 class MetricTracker:
     """用于跟踪tensorboard scalar中的数据的类"""
+
     def __init__(self, *keys, writer=None):
         self.writer = writer
         self._data = pd.DataFrame(index=keys, columns=['total', 'counts', 'average'])
